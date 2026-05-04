61ce993d4a807c6c70cc81b76db3d49d6bfa2c30
viaan-thakur <viaan4542@gmail.com>
2026-05-04 08:26:22 +0000

    sunny: Adapt for @ProjectInfinity-X
    Signed-off-by: viaan-thakur <viaan4542@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/custom_sunny.mk
+    $(LOCAL_DIR)/infinity_sunny.mk
--- infinity_sunny.mk
+++ infinity_sunny.mk
@@ -11,12 +11,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sunny device
 $(call inherit-product, device/xiaomi/sunny/device.mk)
 
-# Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/custom/config/common_full_phone.mk)
+# Inherit some common InfinityX stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := custom_sunny
+PRODUCT_NAME := infinity_sunny
 PRODUCT_DEVICE := sunny
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := M2101K7AG
@@ -27,3 +27,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceName=$(PRODUCT_SYSTEM_NAME)
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# InfinityX Props
+INFINITY_BUILD_TYPE := UNOFFICIAL
+INFINITY_MAINTAINER := "Viaan"
+TARGET_HAS_UDFPS := false
+WITH_GAPPS := true
