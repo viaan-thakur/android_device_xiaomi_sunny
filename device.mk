af5083bda6b45dc0290ddb70aeb2ad1f7b953172
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2026-05-04 08:26:22 +0000

    sunny: Add Dolby Effects in audio_effects.xml
    Co-authored-by: swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
    Signed-off-by: viaan-thakur <viaan4542@gmail.com>

--- device.mk
+++ device.mk
@@ -16,6 +16,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_ven
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# Inherit Dolby Atmos
+$(call inherit-product, hardware/dolby/dolby.mk)
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
